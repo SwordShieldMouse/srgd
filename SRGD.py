cb56f4aa996330cb8ab50f76e0d868f35dd81210
SwordShieldMouse <alke8882000@gmail.com>
2019-07-26 15:59:44 -0600

    renamed hyperparams

--- SRGD.py
+++ SRGD.py
@@ -4,10 +4,10 @@ from torch.optim.optimizer import Optimizer, required
 
 class SRGD(Optimizer):
     """ Implements Stochastic Relativistic Gradient Descent """
-    def __init__(self, params, lr=required, gamma=0.5, m=0.1, c=3e7):
+    def __init__(self, params, lr=required, g=0.5, m=1., c=3e8):
         if lr is not required and lr < 0.0:
             raise ValueError("Invalid learning rate: {}".format(lr))
-        defaults = dict(lr=lr, gamma=gamma, m = m, c = c)
+        defaults = dict(lr=lr, g=g, m = m, c = c)
         super(SRGD, self).__init__(params, defaults)
 
     def __setstate__(self, state):
@@ -25,7 +25,7 @@ class SRGD(Optimizer):
             loss = closure()
 
         for group in self.param_groups:
-            gamma = group['gamma']
+            g = group['g']
             m = group["m"]
             c = group["c"]
 
@@ -39,7 +39,7 @@ class SRGD(Optimizer):
                 if len(state) == 0:
                     state["p"] = torch.zeros_like(p.data)
 
-                state["p"] = math.exp(- group["gamma"] * group["lr"]) * state["p"] - group["lr"] * d_p
+                state["p"] = math.exp(- group["g"] * group["lr"]) * state["p"] - group["lr"] * d_p
 
                 p.data.add_(group['lr'] * group["c"] * state["p"] / torch.sqrt(torch.sum(state["p"] * state["p"]) + math.pow(group["m"] * group["c"], 2) ))
 
